18dedd6fa8fa720f0e021e92f31172a9ae1f1451
unknown <fgp332@gmail.com>
2026-08-10 16:21:54 -0700

    Production seeder

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -10,20 +10,6 @@ class CitySeeder extends Seeder
     public function run(): void
     {
         City::create([
-            'name' => 'Tijuana',
-            'state' => 'Baja California',
-            'country' => 'Mexico',
-            'active' => true,
-        ]);
-
-        City::create([
-            'name' => 'Tecate',
-            'state' => 'Baja California',
-            'country' => 'Mexico',
-            'active' => true,
-        ]);
-
-        City::create([
             'name' => 'Puerto Peñasco',
             'state' => 'Sonora',
             'country' => 'Mexico',

1c08b3fde34b0ae67ac0de2a610fa0e8c8862f96
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-08-15 19:04:33 -0700

    fix(html): add meta tag to html layout so the keyboard won't resize the app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, interactive-widget=overlays-content">
     <title>{{ $title ?? config('app.name') }}</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
